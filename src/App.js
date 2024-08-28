32217ee9fd385a3eaf31176626f10fb02ce62a6b
Dang Le Hai <haidangle678@gmail.com>
2024-08-28 23:23:00 +0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import "./styles.css";
-import Navbar from "./Navbar";
-import Modal from "./Modal";
+
 import TodoApp from "./components/TodoApp";
-import Form from "./Form";
+
 
 const App = () => {
   return (
