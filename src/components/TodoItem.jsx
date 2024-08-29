1899f9319b787b5d05d5f26b70ab9d62b8fbc796
Dang Le Hai <haidangle678@gmail.com>
2024-08-29 22:13:34 +0700

    Update CSS

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,5 @@
+import styles from '../styles/Header.module.css';
+
 const TodoItem = ({ itemProp, setTodos, delTodo }) => {
   const handleChange=(id) =>{
     setTodos((prevState)=>
@@ -12,14 +14,26 @@ const TodoItem = ({ itemProp, setTodos, delTodo }) => {
       })
     );
   }
-    return <li>
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  }
+    return (
+    <li className={styles.item}>
+    <div className={styles.content}>
     <input 
     type="checkbox"
     checked={itemProp.completed}
     onChange={() => handleChange(itemProp.id)}
      />
      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+     <span style={itemProp.completed ? completedStyle : null}>
     {itemProp.title}
-    </li>;
+    </span>
+    </div>
+    </li>
+    );
   };
   export default TodoItem;
