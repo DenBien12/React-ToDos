78e5e43759f642a3336c0ef50691931408cfbd8f
Dang Le Hai <haidangle678@gmail.com>
2024-09-08 21:30:06 +0700

    Update style icon

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa"
 
 const InputTodo=({addTodoItem}) => {
     const [title, setTitle] = useState('');
@@ -27,7 +28,11 @@ const InputTodo=({addTodoItem}) => {
             value={title}
             onChange={handleChange}
             />
-            <button className="input-submit">Submit</button>
+            <button className="input-submit"><FaPlusCircle
+                  color="#5e5e5e"
+                  size="20px"
+                  className="submit-icon"
+            /></button>
         </form>
         <span className="submit-warning">{message}</span>
         </>
